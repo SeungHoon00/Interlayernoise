18eb2d384f1ccc1f3abade1890c73036a6eeadaa
YukSam <yuk6sam3@gmail.com>
2019-01-16 19:10:14 +0900

    sound_ok : 전에 녹음까지 sound_ok2 : 필터링 중

--- sound_ok_2/app/src/main/java/com/example/dlsgh/myapplication3/MainActivity.java
+++ sound_ok_2/app/src/main/java/com/example/dlsgh/myapplication3/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.os.Handler;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -34,23 +36,43 @@ import java.nio.Buffer;
 
 public class MainActivity extends AppCompatActivity {
 
+    private short[] buffer; // 모노라면 여기
+    private short[] mic_top; //스테레오라면 여기
+    private short[] mic_bottom;
+
+    int frequency = 48000;
+    int channelConfiguration = AudioFormat.CHANNEL_IN_STEREO;
+    int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+
+    int recording_block = 50000;  //한번에 녹음하는 sample 수
+
+    short[] mic_sound;
 
     private static String RECORDED_FILE;
     AudioTrack audioTrack;
+    AudioRecord audiorecord;
     MediaPlayer player;
     MediaRecorder recorder;
-    short[] CorrShort;
-    byte generatedSnd[];
+
+    int bufferReadResult = -1;
+    boolean isShake = false;
+    boolean isSwipe = false;
+    boolean isSqueeze = false;
+    boolean record_stop = false;
+
+    String option_s_p[] = {"-r", "range", "train.t", "train.t.scale"}; //svm scale option
+    String option_p[] = {"-b", "1", "train.t.scale", "train.scale.model", "train.t.predict"}; /// svm predict option
+
     short[] Buffer=null;
 
     boolean keepGoing = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         }
 
         super.onCreate(savedInstanceState);
@@ -66,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         Button playStopBtn = (Button) findViewById(R.id.playStopBtn);
         Button playSoundBtn = (Button) findViewById(R.id.PlaysoundBtn);
         Button stopSoundBtn = (Button) findViewById(R.id.StopsoundBtn);
+
 
 
         recordBtn.setOnClickListener(new View.OnClickListener() {
@@ -99,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (recorder == null)
                     return;
-
+                record_stop = true;
                 recorder.stop();
                 recorder.release();
                 recorder = null;
@@ -144,48 +167,7 @@ public class MainActivity extends AppCompatActivity {
         });
         playSoundBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                }
-                if (recorder != null) {
-                    recorder.stop();
-                    recorder.release();
-                    recorder = null;
-                }
-
-                recorder = new MediaRecorder();
-
-                recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-                recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-
-                recorder.setOutputFile(RECORDED_FILE);
-
-                try {
-                    Toast.makeText(getApplicationContext(), "녹음을 시작합니다.", Toast.LENGTH_LONG).show();
-
-                    recorder.prepare();
-                    recorder.start();
-                } catch (Exception ex) {
-                    Log.e("SampleAudioRecorder", "Exception : ", ex);
-                }
-                playSound();
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                }
-                if (recorder == null)
-                    return;
-
-                recorder.stop();
-                recorder.release();
-                recorder = null;
-
-                Toast.makeText(getApplicationContext(), "녹음이 중지되었습니다.", Toast.LENGTH_LONG).show();
-
+               playa();
             }
         });
         stopSoundBtn.setOnClickListener(new View.OnClickListener(){
@@ -209,6 +191,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onPause();
     }
+
     void playSound() {
         Buffer = new short[48000*2];
         if(audioTrack != null) {
@@ -230,6 +213,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     void generatePulse(int freq,int SamplingFreq){
         double omega,time;
         int i,Index=0;
@@ -247,5 +231,121 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void fullsensing(){
+        ////////////////////////////////////////////////
+        //////phase3 : reading PCM file and filtering
+        int array_size;
+        if( channelConfiguration == AudioFormat.CHANNEL_IN_STEREO)
+            array_size = mic_top.length;
+        else
+            array_size = buffer.length;
 
+        mic_sound = new short[array_size];
+
+
+        if( channelConfiguration == AudioFormat.CHANNEL_IN_STEREO) {
+            for (int j = 0; j < array_size; j++) {
+                mic_sound[j] =  mic_top[j];
+            }
+        }
+        else{
+            for(int j=0; j< array_size; j++)
+            {
+                mic_sound[j] = buffer[j];
+            }
+        }
+    }
+
+    void ShowFull_Predict() {
+
+        String r_str;
+        String sd = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String filePath = sd+"/train.pcm";
+        FileOutputStream os = null;
+        try {
+            os = new FileOutputStream(filePath);
+            Toast.makeText(getApplicationContext(), "파일이 추출되었습니다2.", Toast.LENGTH_LONG).show();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        r_str = Integer.toString(-1);
+        try {
+            byte bData[] = short2byte(mic_sound);
+            os.write(bData,0,2048 );
+            Toast.makeText(getApplicationContext(), "파일이 추출되었습니다3.", Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    void playa(){
+        int bufferSize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
+        AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.CAMCORDER, frequency,
+                channelConfiguration, audioEncoding, bufferSize);
+        buffer = new short[recording_block];
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+        }
+        audioRecord.startRecording();
+        Toast.makeText(getApplicationContext(), "녹음을 시작합니다.", Toast.LENGTH_LONG).show();
+
+        //Log.d("SensingService","service_while");
+
+            playSound();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+            }
+            audioRecord.stop();
+        Toast.makeText(getApplicationContext(), "녹음이 중지되었습니다.", Toast.LENGTH_LONG).show();
+
+        bufferReadResult = audioRecord.read(buffer, 0, recording_block); //blockSize = 256
+            if (channelConfiguration == AudioFormat.CHANNEL_IN_STEREO) {
+                mic_top = new short[bufferReadResult / 2];
+                mic_bottom = new short[bufferReadResult / 2];
+                int i = 0;
+                for (int j = 0; j < bufferReadResult; j = j + 2) {
+                    mic_top[i] = buffer[j];
+                    i++;
+                }
+                i = 0;
+                for (int j = 1; j < bufferReadResult; j = j + 2) {
+                    mic_bottom[i] = buffer[j];
+                    i++;
+                }
+            } else {
+                ///streo 녹음 아닐때 처리 일단은 갤럭시는 streo 녹음이기 때문에 처리 안함
+            }
+        fullsensing(); // matched filter 과정
+        Toast.makeText(getApplicationContext(), "matched filter가 완료되었습니다.", Toast.LENGTH_LONG).show();
+
+        ShowFull_Predict(); // FFT 과정 and Sound Signature 추출
+        Toast.makeText(getApplicationContext(), "파일이 추출되었습니다.", Toast.LENGTH_LONG).show();
+    }
+    private byte[] short2byte(short[] sData) {
+
+        int shortArrsize = sData.length;
+
+        byte[] bytes = new byte[shortArrsize * 2];
+
+        for (int i = 0; i < shortArrsize; i++) {
+
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+
+            sData[i] = 0;
+
+        }
+
+        return bytes;
+
+    }
 }
+
